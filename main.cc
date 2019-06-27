f17df78c4dbde2408e65d64751bc3184258082b0
torin <SecretSword2@users.noreply.github.com>
2019-06-27 16:38:55 +0900

    add basic UE

--- main.cc
+++ main.cc
@@ -3,10 +3,13 @@
 
 #define FRAMESIZE 10
 
+#define HEADER 'H'
+#define REQUEST 'Q'
+
 std::array<char, FRAMESIZE> getFrame() {
 	std::array<char, FRAMESIZE> frame = std::array<char, FRAMESIZE>();
-	frame[0] = 'A';
-	frame[8] = 'Z';
+	frame[0] = HEADER;
+	frame[8] = REQUEST;
 	return frame;
 }
 
@@ -15,12 +18,62 @@ void decFrame(std::array<char, FRAMESIZE> frame, int i) {
 	std::cout << data << std::endl;
 }
 
+class UE {
+private:
+	int givenSlot;
+	int currentSlot ;
+	bool canSend;
+	bool isJoined;
+	char data;
+	char sendBuff;
+
+public:
+	void listen(char data) {
+		std::cout << "UE< " <<  data << std::endl;
+		currentSlot = (data == HEADER) ? 0 : currentSlot  + 1;
+		std::cout << currentSlot << std::endl;
+		if (isJoined) {
+			if (currentSlot == givenSlot) {
+				this->data = data;
+				canSend = true;
+			}
+		} else {
+			if (data == REQUEST) {
+				canSend = true;
+			}
+		}
+	}
+
+	void join() {
+		if (!isJoined)
+			sendBuff = REQUEST;
+	}
+
+	void calc() {
+		sendBuff = 'S';
+	}
+
+	char send() {
+		if (!isJoined)
+			join();
+		if (canSend)
+			return sendBuff;
+	}
+};
+
 int main(int argc, char const *argv[]) {
 	std::array<char, FRAMESIZE> frame;
 	frame = getFrame();
+	
+	UE* ue = new UE();
+
 	for (int i = 0; i < FRAMESIZE; i++) {
-		decFrame(frame, i);
+		char& data = frame.at(i);
+		ue->listen(data);
+		std::cout << "UE> " << ue->send() << std::endl;
 	}
+
+	free(ue);
 	
 	return 0;
 }
